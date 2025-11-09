14e6177915e26971a71d25740c07c8bb99827291
Immad <immaduddindurrani@gmail.com>
2025-11-10 00:08:21 +0500

    Improved front end with oracle db on backend

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('destination/', views.destination, name='destination'),
     path('pricing/', views.pricing, name='pricing'),
+    path('search-flights/', views.search_flights, name='search_flights'),
 ]
--- myproject/settings.py
+++ myproject/settings.py
@@ -75,10 +75,13 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'localhost:1521/ORCLPDB',
+        'USER': 'flight_admin',
+        'PASSWORD': 'flight123',
     }
 }
+
 
 
 # Password validation
